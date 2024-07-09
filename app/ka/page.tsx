4da3899bf18b9728e131a4e49b39d0d19c614400
guga-ggw <ggwomega15@gmail.com>
2024-07-09 18:19:11 +0400

    starting structure

---
+++ app/ka/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div className='w-full h-screen'>
+      <div className='w-full h-20 bg-red-800'></div>
+    </div>
+  )
+}
+
+export default MainPage
